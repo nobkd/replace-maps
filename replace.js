8f265ad1c28231928aa68e7f6fd60fb86037016a
nobkd <firewolf6342@gmail.com>
2023-03-23 18:28:00 +0100

    feat: add js load func, todo for other solution

--- replace.js
+++ replace.js
@@ -32,3 +32,21 @@ function convert(url_params) {
 
     return ''; // Full uri for the embed src
 }
+
+
+// loading code to tab
+// this is inefficient, because we then always load the same implementation for every tab
+// TODO: think about implementing a background page instead of this whole script
+function loadJS(url, implementationCode, location) {
+    //url is URL of external file, implementationCode is the code
+    //to be called from the file, location is the location to 
+    //insert the <script> element
+
+    var scriptTag = document.createElement('script');
+    scriptTag.src = url;
+
+    scriptTag.onload = implementationCode;
+    scriptTag.onreadystatechange = implementationCode;
+
+    location.appendChild(scriptTag);
+}
