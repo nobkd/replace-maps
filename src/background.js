d93e8a202a13b1326e59c89f856cd8d3497a6462
nobkd <44443899+nobkd@users.noreply.github.com>
2023-03-27 02:23:28 +0200

    feat: move to new structure & format

--- src/background.js
+++ src/background.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 const patterns = [
     'http://*/maps/embed?*',
     'https://*/maps/embed?*',
     'http://*/maps?*output=embed*',
-    'https://*/maps?*output=embed*'
+    'https://*/maps?*output=embed*',
 ];
 
 const gLocales = ['com', 'de'].join('|'); // TODO: collect more locales
@@ -12,10 +12,11 @@ const matcher = new RegExp(
     `^(https?:\/\/)?(maps\.google\.(${gLocales})\/maps\?.*output=embed|(www\.)?google\.(${gLocales})\/maps\/embed\?)`
 );
 
-
 function redirect(req) {
     if (req.url.match(matcher)) {
-        return { redirectUrl: browser.extension.getURL('map/map.html?' + req.url.split('?')[1]) };
+        return {
+            redirectUrl: browser.extension.getURL('map/map.html?' + req.url.split('?')[1]),
+        };
     }
 }
 
@@ -23,7 +24,7 @@ browser.webRequest.onBeforeRequest.addListener(
     redirect,
     {
         urls: patterns,
-        types: ['sub_frame']
+        types: ['sub_frame'],
     },
     ['blocking']
 );
--- src/map/map.js
+++ src/map/map.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 const params = new URLSearchParams(document.location.search);
 
 const map = L.map('map').setView([51.505, -0.09], 13);
@@ -6,6 +6,6 @@ let marker = L.marker([51.5, -0.09]).addTo(map);
 marker.bindPopup('Position').openPopup();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
