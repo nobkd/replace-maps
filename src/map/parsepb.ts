f7c4885e1f2b756e653e6437978d755e24b532d3
nobkd <44443899+nobkd@users.noreply.github.com>
2023-03-30 20:22:59 +0200

    feat: WIP add ts

---
+++ src/map/parsepb.ts
@@ -0,0 +1,48 @@
+function convertType(item: string): [string | number, boolean] {
+    item = item.replace(/^\d+/, '');
+    const type: string = item.charAt(0);
+    item = item.substring(1);
+
+    // s: string || v: timestamp || b: boolean?/byte?
+    let val: string | number = item;
+
+    switch (type) {
+        case 'f': case 'd': // float || double
+            val = parseFloat(item);
+            break;
+        case 'i': case 'm': // int || matrix1d
+            val = parseInt(item);
+            break;
+        case 'e': // enum
+            switch (item) {
+                case '0':
+                    val = 'roadmap';
+                    break;
+                case '1':
+                    val = 'satellite';
+            };
+            break;
+        case 'z': // base64 encoded coords
+            val = atob(item).replace(/[^\d\s\-\.\'\"\°SNWE]/g, '');
+    };
+
+    return [val, type === 'm'];
+}
+
+export function parsePB(items: string[], out: any[] = []): any[] {
+    let i = 0;
+    while (i < items.length) {
+        let [val, isNew] = convertType(items[i]);
+
+        if (!isNew) {
+            out.push(val);
+        } else {
+            let itemsPart = items.slice(i + 1, i + (val as number) + 1);
+            out.push(parsePB(itemsPart));
+            i += val as number;
+        }
+        i++;
+    }
+
+    return out;
+}
