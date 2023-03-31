20f3bbcb52483091546402d6f2a8e5f5cf10e3b3
nobkd <44443899+nobkd@users.noreply.github.com>
2023-03-31 02:42:53 +0200

    fix: let vite handle the leaflet asset copying

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
-import { defineConfig } from 'vite';
+import { defineConfig, normalizePath } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { resolve } from 'path';
 
 export default defineConfig({
     root: 'src',
     publicDir: '../public',
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: normalizePath(resolve(__dirname, 'node_modules/leaflet/dist/leaflet.css')),
+                    dest: '.',
+                },
+                {
+                    src: normalizePath(resolve(__dirname, 'node_modules/leaflet/dist/images')),
+                    dest: '.',
+                },
+            ],
+        }),
+    ],
     build: {
         outDir: '../.dist',
         emptyOutDir: true,
         rollupOptions: {
             input: {
-                bg: resolve(__dirname, 'src/bg.html'),
-                map: resolve(__dirname, 'src/map.html'),
+                bg: normalizePath(resolve(__dirname, 'src/bg.html')),
+                map: normalizePath(resolve(__dirname, 'src/map.html')),
             },
         },
     },
