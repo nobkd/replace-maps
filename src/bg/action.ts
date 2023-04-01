0168922b816d9ebacce8ae5d019b233a99f3e83f
nobkd <44443899+nobkd@users.noreply.github.com>
2023-04-01 02:39:40 +0200

    feat: trying browser action to disable addon on pages

--- src/bg/action.ts
+++ src/bg/action.ts
@@ -1,5 +1,20 @@
 import browser from 'webextension-polyfill';
 
-browser.browserAction.onClicked.addListener((tab: browser.Tabs.Tab) => {
-    console.log(tab.url);
+browser.browserAction.onClicked.addListener(async (tab: browser.Tabs.Tab) => {
+    console.log(tab);
+
+    /*
+    let disabled: any = await browser.storage.managed.get('disabled');
+    disabled = !(typeof disabled === 'boolean' ? disabled : false);
+
+    if (disabled) {
+        browser.action.enable(tab.id);
+    } else {
+        browser.action.disable(tab.id);
+    }
+
+    browser.storage.managed.set({ disabled: disabled });
+    */
+    
+    browser.tabs.reload(tab.id);
 });
