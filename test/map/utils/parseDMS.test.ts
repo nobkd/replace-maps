1644cb64591a1717d88cf2c790ceff71ec81427f
nobkd <44443899+nobkd@users.noreply.github.com>
2023-04-04 04:06:01 +0200

    test: add parseDMS

--- test/map/utils/parseDMS.test.ts
+++ test/map/utils/parseDMS.test.ts
@@ -1,3 +1,16 @@
 import { describe, it } from 'vitest';
+import { parseDMS } from '../../../src/map/utils/parseDMS';
 
-describe.concurrent('', () => {});
+describe.concurrent('Parse Degrees Minutes Seconds Direction', () => {
+    it('Example', ({ expect }) => {
+        const res = parseDMS(`11°11'11.1"N 11°11'11.1"E`);
+
+        expect(res).toStrictEqual([11.186416666666666, 11.186416666666666]);
+    });
+    
+    it('Negative Example', ({ expect }) => {
+        const res = parseDMS(`11°11'11.1"S 11°11'11.1"W`);
+
+        expect(res).toStrictEqual([-11.186416666666666, -11.186416666666666]);
+    });
+});
