30dc0fbfb01dfd4d97304f1367fe722dbbbdd792
nobkd <44443899+nobkd@users.noreply.github.com>
2023-04-04 02:26:04 +0200

    feat: matching more urls (WIP)

--- src/bg/bg.ts
+++ src/bg/bg.ts
@@ -1,22 +1,22 @@
 import { runtime, webRequest, type WebRequest } from 'webextension-polyfill';
-import { getDisabled } from './utils/storage';
+//import { getDisabled } from './utils/storage';
 
 const patterns: string[] = [
-    'http://*/maps/embed?*',
-    'https://*/maps/embed?*',
-    'http://*/maps?*output=embed*',
-    'https://*/maps?*output=embed*',
+    'http://*/maps/embed*?*',
+    'https://*/maps/embed*?*',
+    'http://*/maps*?*output=embed*',
+    'https://*/maps*?*output=embed*',
 ];
 
 const gLocales: string = ['com', 'de'].join('|'); // TODO: collect more locales
 const matcher: RegExp = new RegExp(
     // TODO: fix regex to fit more patterns
-    `^(https?:\/\/)?(maps\.google\.(${gLocales})\/maps\?.*output=embed|(www\.)?google\.(${gLocales})\/maps\/embed\?)`
+    `^(https?:\/\/)?(maps\.google\.(${gLocales})\/maps.*\?.*output=embed|(www\.)?google\.(${gLocales})\/maps\/embed.*\?)`
 );
 
-function redirect(req: WebRequest.OnBeforeRequestDetailsType): WebRequest.BlockingResponse {
+/*async*/ function redirect(req: WebRequest.OnBeforeRequestDetailsType): /*Promise<*/WebRequest.BlockingResponse/*>*/ {
     if (req.originUrl && req.url.match(matcher)) {
-        //if (!getDisabled(req.originUrl)) { // TODO: resolve promise async, but response of fuc has to be sync
+        //if (!await getDisabled(req.originUrl)) {
         return {
             redirectUrl: runtime.getURL('map.html?' + req.url.split('?')[1]),
         };
