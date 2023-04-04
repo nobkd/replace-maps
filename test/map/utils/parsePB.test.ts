50e48acef6aea258bb7ec9b5a8289c3c3ba01950
nobkd <44443899+nobkd@users.noreply.github.com>
2023-04-04 16:25:30 +0200

    test: fix to use better test statements

--- test/map/utils/parsePB.test.ts
+++ test/map/utils/parsePB.test.ts
@@ -16,57 +16,56 @@ describe.concurrent('Parse PB', () => {
     });
 
     it('double', ({ expect }) => {
-        const res = parsePB(['1d1.1'])[0];
+        const res = parsePB(['1d1.1']);
 
-        expect(res).toBeTypeOf('number');
-        expect(res).toBe(1.1);
+        expect(res[0]).toBeTypeOf('number');
+        expect(res).toContain(1.1);
     });
 
     it('float', ({ expect }) => {
-        const res = parsePB(['1f1.1'])[0];
+        const res = parsePB(['1f1.1']);
 
-        expect(res).toBeTypeOf('number');
-        expect(res).toBe(1.1);
+        expect(res[0]).toBeTypeOf('number');
+        expect(res).toContain(1.1);
     });
 
     it('int', ({ expect }) => {
-        const res = parsePB(['1i1'])[0];
+        const res = parsePB(['1i1']);
 
-        expect(res).toBeTypeOf('number');
-        expect(res).toBe(1);
-        expect(res).to;
+        expect(res[0]).toBeTypeOf('number');
+        expect(res).toContain(1);
     });
 
     it('enum roadmap', ({ expect }) => {
         const res = parsePB(['1e0'])[0];
 
-        expect(res).toBeTypeOf('string');
-        expect(res).toBe('roadmap');
-        expect(res).toBe(tileTypes[0]);
+        expect(res[0]).toBeTypeOf('string');
+        expect(res).toContain('roadmap');
+        expect(res).toContain(tileTypes[0]);
     });
 
     it('enum satellite', ({ expect }) => {
-        const res = parsePB(['1e1'])[0];
+        const res = parsePB(['1e1']);
 
-        expect(res).toBeTypeOf('string');
-        expect(res).toBe('satellite');
-        expect(res).toBe(tileTypes[1]);
+        expect(res[0]).toBeTypeOf('string');
+        expect(res).toContain('satellite');
+        expect(res).toContain(tileTypes[1]);
     });
 
     it('enum empty', ({ expect }) => {
-        const res = parsePB(['1e'])[0];
+        const res = parsePB(['1e']);
 
-        expect(res).toBeTypeOf('string');
-        expect(res).toBe('roadmap');
-        expect(res).toBe(tileTypes[0]);
+        expect(res[0]).toBeTypeOf('string');
+        expect(res).toContain('roadmap');
+        expect(res).toContain(tileTypes[0]);
     });
 
     it('enum >1', ({ expect }) => {
-        const res = parsePB(['1e2'])[0];
+        const res = parsePB(['1e2']);
 
-        expect(res).toBeTypeOf('string');
-        expect(res).toBe('roadmap');
-        expect(res).toBe(tileTypes[0]);
+        expect(res[0]).toBeTypeOf('string');
+        expect(res).toContain('roadmap');
+        expect(res).toContain(tileTypes[0]);
     });
 
     it('wrongly encoded base64', ({ expect }) => {
