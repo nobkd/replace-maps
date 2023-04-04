fd10f3eec7a96a4be12eff4cd748ed5d8530a1be
nobkd <44443899+nobkd@users.noreply.github.com>
2023-04-04 04:12:09 +0200

    test: add zoom (WIP)

--- test/map/utils/zoom.test.ts
+++ test/map/utils/zoom.test.ts
@@ -1,3 +1,18 @@
 import { describe, it } from 'vitest';
+import { getMapZoom } from '../../../src/map/utils/zoom';
 
-describe.concurrent('', () => {});
+describe.concurrent('', () => {
+    it('zoom > 19', ({expect}) => {
+        const res = getMapZoom(1);
+        
+        expect(res).toBeTypeOf('number');
+        expect(res).toBe(19);
+    });
+
+    it('zoom < 0', ({expect}) => {
+        const res = getMapZoom(100000000);
+        
+        expect(res).toBeTypeOf('number');
+        expect(res).toBe(0);
+    });
+});
