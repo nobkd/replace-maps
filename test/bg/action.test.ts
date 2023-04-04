0ef4e030507b9d667cb02cd8ea50f2970e0a4906
nobkd <44443899+nobkd@users.noreply.github.com>
2023-04-04 16:15:35 +0200

    chore: formatting

--- test/bg/action.test.ts
+++ test/bg/action.test.ts
@@ -1,5 +1,3 @@
 import { describe, it } from 'vitest';
 
-describe.concurrent('', () => {
-
-});
+describe.concurrent('', () => {});
--- test/map/utils/zoom.test.ts
+++ test/map/utils/zoom.test.ts
@@ -2,16 +2,16 @@ import { describe, it } from 'vitest';
 import { getMapZoom } from '../../../src/map/utils/zoom';
 
 describe.concurrent('', () => {
-    it('zoom > 19', ({expect}) => {
+    it('zoom > 19', ({ expect }) => {
         const res = getMapZoom(1);
-        
+
         expect(res).toBeTypeOf('number');
         expect(res).toBe(19);
     });
 
-    it('zoom < 0', ({expect}) => {
+    it('zoom < 0', ({ expect }) => {
         const res = getMapZoom(100000000);
-        
+
         expect(res).toBeTypeOf('number');
         expect(res).toBe(0);
     });
