6d9cebe0d1e968b12226678c9b900c7b5aa730cf
nobkd <44443899+nobkd@users.noreply.github.com>
2023-04-16 03:17:14 +0200

    fix: WIP: crrently one directional, reload frames instead of tab

--- src/bg/action.ts
+++ src/bg/action.ts
@@ -1,15 +1,27 @@
-import { browserAction, tabs, type Tabs } from 'webextension-polyfill';
+import { browserAction, webNavigation, type Tabs, tabs } from 'webextension-polyfill';
 import { getHostname, invertHostState } from './utils/storage';
 import { updateIcon } from './utils/actionIcon';
+import { matcher, runtimeMapUrl } from './bg';
+
+const replacedUrlMatcher = new RegExp(`^${runtimeMapUrl}\?`);
 
 browserAction.onClicked.addListener(async (tab: Tabs.Tab) => {
-    if (!tab.url) return;
+    if (!tab.url || !tab.id) return;
 
     let hostname = getHostname(tab.url);
     await invertHostState(hostname);
 
     updateIcon(hostname);
 
-    // TODO: reload only frames (that have gmaps url), not full page
-    tabs.reload(tab.id, { bypassCache: true });
+    let frames = (await webNavigation.getAllFrames({ tabId: tab.id })) ?? [];
+    frames = frames.filter((frame) => {
+        return frame.url.match(matcher) || frame.url.match(replacedUrlMatcher);
+    });
+    frames.forEach((frame) => {
+        // TODO: currently only works "from maps to osm" and "not from osm to maps" => fix it...
+        tabs.executeScript(tab.id, {
+            frameId: frame.frameId,
+            code: 'document.location.reload();',
+        });
+    });
 });
--- src/bg/bg.ts
+++ src/bg/bg.ts
@@ -10,16 +10,18 @@ const patterns: string[] = [
 ];
 
 const gLocales: string = ['com', 'de'].join('|'); // TODO: collect more locales
-const matcher: RegExp = new RegExp(
+export const matcher: RegExp = new RegExp(
     // TODO: fix regex to fit more patterns
     `^(https?:\/\/)?(maps\.google\.(${gLocales})\/maps.*\?.*output=embed|(www\.)?google\.(${gLocales})\/maps\/embed.*\?)`
 );
+export const runtimeMapUrl = runtime.getURL('map.html');
 
 function redirect(req: WebRequest.OnBeforeRequestDetailsType): WebRequest.BlockingResponse {
+    // TODO: check if originUrl always matches current tab url -> e.g. in frames with subframes
     if (req.originUrl && req.url.match(matcher)) {
         if (!disabledHosts.includes(getHostname(req.originUrl))) {
             return {
-                redirectUrl: runtime.getURL('map.html?' + req.url.split('?')[1]),
+                redirectUrl: runtimeMapUrl + '?' + req.url.split('?')[1],
             };
         }
     }
