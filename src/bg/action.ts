3dc212ca886527339d2de9af114975750fbd4490
nobkd <44443899+nobkd@users.noreply.github.com>
2023-07-15 00:03:47 +0200

    fix: reload tab on maps or extension leaflet match

--- src/bg/action.ts
+++ src/bg/action.ts
@@ -1,6 +1,6 @@
 import { browserAction, webNavigation, type Tabs, tabs } from 'webextension-polyfill';
 import { getHostname, invertHostState } from './utils/storage';
-import { matcher, runtimeMapUrl } from './bg';
+import { matcher as mapsUrlMatcher, runtimeMapUrl } from './bg';
 
 const replacedUrlMatcher = new RegExp(`^${runtimeMapUrl}\?`);
 
@@ -9,8 +9,7 @@ const replacedUrlMatcher = new RegExp(`^${runtimeMapUrl}\?`);
  * Takes the current tab, takes its hostname and inverts the state of the hostname.
  *
  * Requests all frames from the current tab, filters them for extension Leaflet frames and Maps frames.
- * Reloads the full tab on extension Leaflet frame match.
- * If no extension Leaflet frames were found it just reloads the Maps frames
+ * Reloads the full tab on extension Leaflet or Maps frame match.
  * @param tab Currently active tab
  */
 async function actionClick(tab: Tabs.Tab): Promise<void> {
@@ -21,29 +20,13 @@ async function actionClick(tab: Tabs.Tab): Promise<void> {
 
     let frames = (await webNavigation.getAllFrames({ tabId: tab.id })) ?? [];
 
-    // Added instead of commented below, as map currently isn't loaded properly on just frame reload...
-    if (frames.some((frame) => frame.url.match(replacedUrlMatcher) || frame.url.match(matcher))) {
+    if (
+        frames.some((frame) => {
+            frame.url.match(replacedUrlMatcher) || frame.url.match(mapsUrlMatcher);
+        })
+    ) {
         tabs.reload(tab.id, { bypassCache: true });
-        return;
     }
-
-    // TODO: check if reusable
-    /*
-    // matches osm frames (just reloading the frame ist not working for some reason, so in case of replaced maps the whole tab is reloaded)
-    if (frames.some((frame) => frame.url.match(replacedUrlMatcher))) {
-        tabs.reload(tab.id, { bypassCache: true });
-        return;
-    }
-
-    // matches maps frames (finds the frames that have maps and reloads only them)
-    frames = frames.filter((frame) => frame.url.match(matcher));
-    frames.forEach((frame) => {
-        tabs.executeScript(tab.id, {
-            frameId: frame.frameId,
-            code: 'document.location.reload();',
-        });
-    });
-    */
 }
 
 browserAction.onClicked.addListener(actionClick);
